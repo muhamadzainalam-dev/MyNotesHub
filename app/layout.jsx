37967efbbf8c1d383d32f7c124204e8fe2a6e0fb
DigitalMonsterr <digitolmonster@gmail.com>
2025-02-05 23:49:57 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,17 +27,8 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4728036164112649"
           crossorigin="anonymous"
         ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-4728036164112649",
-            enable_page_level_ads: true
-          });
-        `,
-          }}
-        />
       </Head>
+
       <body>
         <header>
           <Header />
