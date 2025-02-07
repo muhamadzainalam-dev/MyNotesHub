78505c1fca4404c265d93e5e3dbb2741763f3a80
DigitalMonsterr <digitolmonster@gmail.com>
2025-02-07 22:56:48 +0500

    ADDING ADSENSE ACCOUNT

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import { Header } from "@/components/ui/header";
 import Footer from "@/components/custom/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
@@ -20,7 +21,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* Google AdSense Script */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8503201959339230"
+          crossOrigin="anonymous"
+          strategy="afterInteractive" 
+        />
+        <Script id="adsbygoogle-init" strategy="afterInteractive">
+          {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+        </Script>
+
         <header>
           <Header />
         </header>
@@ -28,6 +40,7 @@ export default function RootLayout({ children }) {
         <footer>
           <Footer />
         </footer>
+
         <Toaster />
         <SpeedInsights />
         <Analytics />
