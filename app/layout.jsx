a2d269ac15c845c29c38313aa685466d54003dfe
DigitalMonsterr <digitolmonster@gmail.com>
2025-02-13 15:55:27 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -50,13 +50,15 @@ export default function RootLayout({ children }) {
 
         <MobileSidebar />
         <main>
-          {pathname !== "/pages/AboutUs" && (
-            <div className="bg-gray-100 px-4 pb-6">
-              <div className="h-auto">
-                <GoogleAd slot="4228674601" />
+          <div>
+            {pathname !== "/pages/AboutUs" && (
+              <div className="bg-gray-100 px-4 pb-6">
+                <div className="h-auto">
+                  <GoogleAd slot="4228674601" />
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
           {children}
           {pathname !== "/pages/AboutUs" && (
             <div className="bg-gray-100 px-4 pb-6">
